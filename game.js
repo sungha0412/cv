f0a15cfc09309f5e9f1cc9d210636ddb79fb908c
sungha0412 <69449338+sungha0412@users.noreply.github.com>
2021-01-22 19:20:22 +0900

    Update game.js

--- game.js
+++ game.js
@@ -7,7 +7,7 @@ var userClickedPattern = [];
 var started = false;
 var level = 0;
 
-$("h1").click(function() {
+$("#level-title").click(function() {
   if (!started) {
     $("#level-title").text("Level " + level);
     nextSequence();
